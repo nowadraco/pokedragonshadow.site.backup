a130d395b3e1431c5338e220f7411a9d15ae93b6
Luiz José SIlvestre <80053389+nowadraco@users.noreply.github.com>
2024-10-13 16:35:51 -0300

    configurando menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,9 @@
 function setupMenuToggle() {
     const menuHamburguer = document.querySelector('.menu-hamburguer');
     const menu = document.querySelector('.menu');
-
     menuHamburguer.addEventListener('click', () => {
         menu.classList.toggle('active');
+        menuHamburguer.classList.toggle('active');
     });
 }
 
