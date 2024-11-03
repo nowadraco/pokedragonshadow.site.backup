277228a666b11e5f8fafdc4efe7fb9ed4e6f98e1
nowadraco <Nowadraco@gmail.com>
2024-11-03 00:15:16 +0000

    voltar ao inicio

---
+++ src/scripts/voltarAoTop.js
@@ -0,0 +1,16 @@
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
