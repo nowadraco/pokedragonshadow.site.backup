d710990bdff4084c01e42a97307e0a1e24b2fd83
nowadraco <Nowadraco@gmail.com>
2024-11-03 00:06:37 +0000

    estilizando logo

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,14 @@
 function setupMenuToggle() {
     const menuHamburguer = document.querySelector('.menu-hamburguer');
     const menu = document.querySelector('.menu');
+    const logos = document.querySelector('.logos');
+    const header = document.querySelector('header');
+
     menuHamburguer.addEventListener('click', () => {
         menu.classList.toggle('active');
         menuHamburguer.classList.toggle('active');
+        logos.classList.toggle('hidden');
+        header.classList.toggle('no-justify-space-between');
     });
 }
 
