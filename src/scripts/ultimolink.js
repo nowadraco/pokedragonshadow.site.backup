8fd47db0cf6502a6532eb9efb5235dcdc88ef6c7
nowadraco <Nowadraco@gmail.com>
2025-01-03 22:15:53 +0000

    caminho

--- src/scripts/ultimolink.js
+++ src/scripts/ultimolink.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', async (event) => {
-    await updateLink('../src/paginas/gorocket/', 'a#ultimoPostGorocket');
-    await updateLink('../src/paginas/reides/', 'a#ultimoPostReide');
+    await updateLink('/src/paginas/gorocket/', 'a#ultimoPostGorocket');
+    await updateLink('./src/paginas/reides/', 'a#ultimoPostReide');
     await updateLink('../src/paginas/pesquisa-de-campo/', 'a#ultimoPostpesquisas');
-    await updateLink('../src/paginas/pvp/', 'a#ultimoPostPvp');
+    await updateLink('../../src/paginas/pvp/', 'a#ultimoPostPvp');
 });
 
 async function updateLink(directory, selector) {
