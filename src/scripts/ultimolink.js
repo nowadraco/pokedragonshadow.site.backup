8dfe5aeb20a9c2e52c465f3fb5a9c6a572c897cd
nowadraco <Nowadraco@gmail.com>
2025-01-14 18:57:14 +0000

    add arquivos json

--- src/scripts/ultimolink.js
+++ src/scripts/ultimolink.js
@@ -1,43 +1,25 @@
 document.addEventListener('DOMContentLoaded', async (event) => {
-    await updateLink('src/paginas/gorocket/', 'a#ultimoPostGorocket');
-    await updateLink('src/paginas/reides/', 'a#ultimoPostReide');
-    await updateLink('src/paginas/pesquisa-de-campo/', 'a#ultimoPostpesquisas');
-    await updateLink('src/paginas/pvp/', 'a#ultimoPostPvp');
+    await updateLink('https://nowadraco.github.io/pokedragonshadow.site/src/json_files/pagesGoRocket.json', 'a#ultimoPostGorocket');
+    await updateLink('https://nowadraco.github.io/pokedragonshadow.site/src/json_files/pagesReide.json', 'a#ultimoPostReide');
+    await updateLink('https://nowadraco.github.io/pokedragonshadow.site/src/json_files/pagesPesquisaDeCampo.json', 'a#ultimoPostpesquisas');
+    await updateLink('https://nowadraco.github.io/pokedragonshadow.site/src/json_files/pagesPvp.json', 'a#ultimoPostPvp');
 });
 
-async function updateLink(directory, selector) {
+async function updateLink(jsonFile, selector) {
     try {
-        const response = await fetch(directory);
+        const response = await fetch(jsonFile);
         if (!response.ok) {
-            throw new Error(`Erro ao carregar o diretório de ${directory}`);
+            throw new Error(`Erro ao carregar o arquivo ${jsonFile}`);
         }
-        const text = await response.text();
-        const sortedLinks = parseAndSortLinks(text);
+        const jsonData = await response.json();
+        const latestPost = jsonData[0].link; // Assume-se que há uma lista de objetos no JSON
 
-        const latestPost = sortedLinks[0];
         const linkElement = document.querySelector(selector);
 
         if (linkElement) {
-            linkElement.setAttribute('href', `${latestPost}`);
+            linkElement.setAttribute('href', latestPost);
         }
     } catch (error) {
-        console.error(`Erro ao buscar os posts em ${directory}:`, error);
+        console.error(`Erro ao buscar os posts no arquivo ${jsonFile}:`, error);
     }
-}
-
-function parseAndSortLinks(text) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(text, 'text/html');
-    const links = [...doc.querySelectorAll('a')];
-
-    return links
-    .map(link => `./${link.getAttribute('href')}`)
-    .filter(href => href.match(/\d{4}-\d{2}-\d{2}-\d{2}-\d{2}\.html/))
-    .sort((a, b) => {
-        const aParts = a.match(/(\d{4})-(\d{2})-(\d{2})-(\d{2})-(\d{2})/).slice(1);
-        const bParts = b.match(/(\d{4})-(\d{2})-(\d{2})-(\d{2})-(\d{2})/).slice(1);
-        const aDate = new Date(aParts[0], aParts[2] - 1, aParts[1], aParts[3], aParts[4]);
-        const bDate = new Date(bParts[0], bParts[2] - 1, bParts[1], bParts[3], bParts[4]);
-        return bDate - aDate;
-    });
 }
